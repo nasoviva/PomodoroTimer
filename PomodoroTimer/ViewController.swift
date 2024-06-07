73aa309757533d814d3bb362bb73b39c06a7013d
nasoviva <nasoviva@gmail.com>
2024-06-07 15:37:55 -0300

    add new constraints

--- PomodoroTimer/ViewController.swift
+++ PomodoroTimer/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var labelText: UILabel!
+
+    let textOfLabel = ["Decide on the task to be completed.",
+    "Set a timer for 25 minutes, the length of one pomodoro.",
+    "Work on the task until the timer rings, then put a checkmark on a piece of paper.",
+    "Take a short break (usually 5 minutes) to stretch and relax.",
+    "After four pomodoros, take a longer break (usually 15-30 minutes)."]
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
